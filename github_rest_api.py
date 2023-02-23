3f7ce65a83688a282d6aff52dd6355be0822ec7b
lbrealdev <lbrealdeveloper@gmail.com>
2023-02-24 00:35:06 +0100

    feat: refactor github get user function

--- github_rest_api.py
+++ github_rest_api.py
@@ -2,15 +2,15 @@ import requests
 import pprint
 import os
 
-GH_API_URL = "https://api.github.com/repos/lbrealdev/azure"
+GH_API_URL = "https://api.github.com"
 GH_AUTH_TOKEN = os.environ.get('GH_API_TOKEN')
 
-def gh_get_repository():
-    resp = requests.get(GH_API_URL, headers={"Authorization": "token {}".format(GH_AUTH_TOKEN)})
-    if resp.status_code == requests.codes.okay:
+def github_get_user(username: str):
+    resp = requests.get(f'{GH_API_URL}/users/{username}', headers={"Authorization": "token {}".format(GH_AUTH_TOKEN)})
+    if resp.status_code == 200:
         pprint.pprint(resp.json())
     else:
         print("Failed!")
 
 if __name__ == '__main__':
-    gh_get_repository()
+    github_get_user(username="lbrealdev")
