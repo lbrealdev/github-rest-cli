4d66c7b12869efd97277c40c3be5da17384c8ca0
lbrealdev <lbrealdeveloper@gmail.com>
2023-02-26 15:37:49 +0100

    feat: add argparse python module

--- github-rest-api.py
+++ github-rest-api.py
@@ -1,13 +1,14 @@
 import requests
 import pprint
 import os
+import argparse
 
 GH_API_URL = "https://api.github.com"
 GH_AUTH_TOKEN = os.environ.get('GH_API_TOKEN')
 
-def github_get_user(username: str):
+def github_user_data(user: str):
     resp = requests.get(
-            f'{GH_API_URL}/users/{username}',
+            f'{GH_API_URL}/users/{user}',
             headers={"Authorization": f'token {GH_AUTH_TOKEN}'}
             )
     if resp.status_code == 200:
@@ -15,5 +16,18 @@ def github_get_user(username: str):
     else:
         print("Failed!")
 
+def main():
+    parser = argparse.ArgumentParser(
+                'Github REST API',
+                description='Python CLI to Github REST API',
+                )
+    parser.add_argument('-u', '--user',
+                        help='Github user',
+                        required=True,
+                        dest='user')
+    
+    args = parser.parse_args()
+    github_user_data(args.user)
+
 if __name__ == '__main__':
-    github_get_user(username="lbrealdev")
+    main()
