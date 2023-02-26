b1446b64616a57ab84577aef4a6d9ad9ac61ce53
lbrealdev <lbrealdeveloper@gmail.com>
2023-02-26 15:24:41 +0100

    feat: rename python file and add ruff linter

--- github-rest-api.py
+++ github-rest-api.py
@@ -6,7 +6,10 @@ GH_API_URL = "https://api.github.com"
 GH_AUTH_TOKEN = os.environ.get('GH_API_TOKEN')
 
 def github_get_user(username: str):
-    resp = requests.get(f'{GH_API_URL}/users/{username}', headers={"Authorization": "token {}".format(GH_AUTH_TOKEN)})
+    resp = requests.get(
+            f'{GH_API_URL}/users/{username}',
+            headers={"Authorization": f'token {GH_AUTH_TOKEN}'}
+            )
     if resp.status_code == 200:
         pprint.pprint(resp.json())
     else:
