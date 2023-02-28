1d088eed3e5ecd7048fc268bff975a42cc2468c9
lbrealdev <lbrealdeveloper@gmail.com>
2023-02-28 01:51:02 +0100

    feat: making new subparser to work - delete-repository

--- github-rest-api.py
+++ github-rest-api.py
@@ -3,7 +3,7 @@ import argparse
 from config import settings
 
 
-def github_check_repository(name):
+def check_repository(name):
     headers = {
        "Authorization": f"token {settings.AUTH_TOKEN}",
        "Accept": "application/vnd.github.v3+json"
@@ -13,50 +13,93 @@ def github_check_repository(name):
         headers=headers
     )
     if resp.status_code == 200:
+        print("The repository already exists!")
         return True
     else:
         return False
 
-def github_create_repository(name):
+def create_repository(name):
     headers = {
         "Authorization": f"token {settings.AUTH_TOKEN}",
         "Accept": "application/vnd.github.v3+json"
     }
-    payload = {
-        "name": f"{name}",
-        "auto_init": True,
-        "private": True,
+    data = {
+        "name": name,
+        "auto_init": "true",
+        "private": "true",
     }
     resp = requests.post(
         f'{settings.API_URL}/user/repos',
         headers=headers,
-        json=payload
+        json=data
     )
     if resp.status_code == 201:
         print("Repository created sucessfully!")
     else:
         print(f"Failed to create repository {name} with status code {resp.status_code}")
+        return False
+
+def delete_repository(name):
+    headers = {
+       "Authorization": f"token {settings.AUTH_TOKEN}",
+       "Accept": "application/vnd.github.v3+json"
+    }
+    resp = requests.delete(
+        f'{settings.API_URL}/repos/{settings.USER}/{name}',
+        headers=headers
+    )
+    if resp.status_code == 204:
+        print("Repository deleted!")
+    elif resp.status_code == 404:
+        print('Repository not found!')
+    else:
+        print(f"Failed to delete repository {name} with status code {resp.status_code}")
+        return False
 
 def main():
+    # top-level parser
     parser = argparse.ArgumentParser(
-                'Python Github REST API',
+                prog='Python Github REST API',
                 description='Python CLI to Github REST API',
                 )
-    subparsers = parser.add_subparsers(help="Python Github REST API subcommands")
-    parser_repository = subparsers.add_parser('new-repository',
-                                              help='Create new repository')
-    parser_repository.add_argument('-n', 
-                                   '--name',
-                                   help='The repository name',
-                                   required=True,
-                                   dest='name')
+    subparsers = parser.add_subparsers(
+                                    help="Python Github REST API commands",
+                                    dest='command')
+    
+    # create-repository function parser
+    parser_create_repository = subparsers.add_parser(
+                                    'create-repository',
+                                    help='Create new repository')
+    parser_create_repository.add_argument(
+                                    '-n', 
+                                    '--name',
+                                    help='Name for new repository',
+                                    required=True,
+                                    dest='name')
+    
+    # delete-repository function parser
+    parser_delete_repository = subparsers.add_parser(
+                                    'delete-repository',
+                                    help='Delete repository')
+    parser_delete_repository.add_argument(
+                                    '-n',
+                                    '--name',
+                                    help='Repository name to delete',
+                                    required=True,
+                                    dest='name')
     
     args = parser.parse_args()
 
-    if github_check_repository(args.name):
-        print("The repository already exists!")
+    if args.command == "create-repository":
+        verify = check_repository(args.name)
+        if verify is False:
+            create_repository(args.name)
+        else:
+            pass
+    elif args.command == "delete-repository":
+        delete_repository(args.name)
     else:
-        github_create_repository(args.name)
+        return False
 
 if __name__ == '__main__':
     main()
