f8cbd810c08496a8a3f8c2e95fed1984cc36564e
lbrealdev <lbrealdeveloper@gmail.com>
2023-02-28 19:00:05 +0100

    feat: refactor code and apply black python

--- config.py
+++ config.py
@@ -3,18 +3,18 @@
 from dynaconf import Dynaconf, Validator
 
 settings = Dynaconf(
-    # Default environment variables prefix. 
+    # Default environment variables prefix.
     envvar_prefix="GITHUB",
     # Source configuration files.
-    settings_files=['settings.toml', '.secrets.toml'],
+    settings_files=["settings.toml", ".secrets.toml"],
     environments=["development", "testing", "production"],
     env_switcher="SET_ENV",
     # The script will not work if the variables
     # defined in the Validator class are not defined.
     validators=[
-        Validator("API_URL", must_exist=True, cont='github') &
-        Validator("AUTH_TOKEN", must_exist=True),
-    ]
+        Validator("API_URL", must_exist=True, cont="github")
+        & Validator("AUTH_TOKEN", must_exist=True),
+    ],
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
--- github-rest-api.py
+++ github-rest-api.py
@@ -12,80 +12,75 @@ console = Console()
 def get_repository(name):
     headers = {
         "X-GitHub-Api-Version": "2022-11-28",
-        "Authorization": f"token {settings.AUTH_TOKEN}"
+        "Authorization": f"token {settings.AUTH_TOKEN}",
     }
     resp = requests.get(
-        f'{settings.API_URL}/repos/{settings.USER}/{name}',
-        headers=headers
+        f"{settings.API_URL}/repos/{settings.USER}/{name}", headers=headers
     )
     if resp.status_code == 200:
         source_repo = json.loads(resp.text)
         print(source_repo)
     elif resp.status_code == 404:
         console.print(
-                "The requested repository does not exist!",
-                style='blink bold red')
+            "The requested repository does not exist!", style="blink bold red"
+        )
     else:
         console.print(
             f"Failed to get repository {name} with status code {resp.status_code}",
-            style='blink bold red')
+            style="blink bold red",
+        )
         return False
+
 
 def create_repository(name):
     headers = {
         "X-GitHub-Api-Version": "2022-11-28",
-        "Authorization": f"token {settings.AUTH_TOKEN}"
+        "Authorization": f"token {settings.AUTH_TOKEN}",
     }
     data = {
         "name": name,
         "auto_init": "true",
         "private": "true",
     }
-    resp = requests.post(
-        f'{settings.API_URL}/user/repos',
-        headers=headers,
-        json=data
-    )
+    resp = requests.post(f"{settings.API_URL}/user/repos", headers=headers, json=data)
     if resp.status_code == 201:
         console.print("Repository created sucessfully!", style="blink bold green")
     else:
         console.print(
             f"Failed to create repository {name} with status code {resp.status_code}",
-            style='blink bold red')
+            style="blink bold red",
+        )
         return False
+
 
 def delete_repository(name):
     headers = {
         "X-GitHub-Api-Version": "2022-11-28",
-        "Authorization": f"token {settings.AUTH_TOKEN}"
+        "Authorization": f"token {settings.AUTH_TOKEN}",
     }
     resp = requests.delete(
-        f'{settings.API_URL}/repos/{settings.USER}/{name}',
-        headers=headers
+        f"{settings.API_URL}/repos/{settings.USER}/{name}", headers=headers
     )
     if resp.status_code == 204:
-        console.print("Repository deleted!", style='blink bold green')
+        console.print("Repository deleted!", style="blink bold green")
     elif resp.status_code == 404:
-        console.print("Repository not found!", style='blink bold red')
+        console.print("Repository not found!", style="blink bold red")
     else:
         console.print(
             f"Failed to delete repository {name} with status code {resp.status_code}",
-            style='blink bold red')
+            style="blink bold red",
+        )
         return False
 
-def list_repository(username):
+
+def list_repository():
     headers = {
         "X-GitHub-Api-Version": "2022-11-28",
-        "Authorization": f"token {settings.AUTH_TOKEN}"
+        "Authorization": f"token {settings.AUTH_TOKEN}",
     }
-    params = {
-        'per_page': '100',
-        'sort': 'updated'
-    }
+    params = {"per_page": "100", "sort": "updated"}
     resp = requests.get(
-        f'{settings.API_URL}/users/{username}/repos',
-        headers=headers,
-        params=params
+        f"{settings.API_URL}/user/repos", headers=headers, params=params
     )
     if resp.status_code == 200:
         repos = json.loads(resp.text)
@@ -94,83 +89,69 @@ def list_repository(username):
             print(f"- {repo_name}")
     else:
         console.print(
-            f"Failed to list repositories for {username} with status code {resp.status_code}",
-            style='blink bold red')
+            f"Failed to list repositories for {settings.USER}\
+                with status code {resp.status_code}",
+            style="blink bold red",
+        )
         return False
 
 
 def main():
     # top-level parser
     parser = argparse.ArgumentParser(
-                prog='Python Github REST API',
-                description='Python CLI to Github REST API',
-                )
+        prog="Python Github REST API",
+        description="Python CLI to Github REST API",
+    )
     subparsers = parser.add_subparsers(
-                                    help="Python Github REST API commands",
-                                    dest='command')
-    
-    # create-repository function parser
-    parser_create_repository = subparsers.add_parser(
-        'create-repository',
-        help='Create new repository'
-    )
-    parser_create_repository.add_argument(
-        '-n', '--name',
-        help='Name for new repository',
-        required=True,
-        dest='name'
-    )
-    
-    # delete-repository function parser
-    parser_delete_repository = subparsers.add_parser(
-        'delete-repository',
-        help='Delete repository'
-    )
-    parser_delete_repository.add_argument(
-        '-n', '--name',
-        help='Repository name to delete',
-        required=True,
-        dest='name'
+        help="Python Github REST API commands", dest="command"
     )
 
+    # get-repository function parser
     parser_get_repository = subparsers.add_parser(
-        'get-repository',
-        help='Get repository data'
+        "get-repository", help="Get repository data for authenticated user"
     )
     parser_get_repository.add_argument(
-        '-n', '--name',
-        help='Get a specific repository from github',
+        "-n",
+        "--name",
+        help="Get a specific repository from github",
         required=True,
-        dest='name'
+        dest="name",
     )
-    
+
     # list-repository function parser
-    parser_list_repository = subparsers.add_parser(
-        'list-repository',
-        help='Lists public repositories for the specified user'
+    subparsers.add_parser(
+        "list-repository", help="List repositories for authenticated user"
     )
-    parser_list_repository.add_argument(
-        '-u',
-        '--username',
-        help='Github username to list repositories',
-        required=True,
-        dest='username'
+
+    # create-repository function parser
+    parser_create_repository = subparsers.add_parser(
+        "create-repository", help="Create new repository"
     )
-    
-    
+    parser_create_repository.add_argument(
+        "-n", "--name", help="Name for new repository", required=True, dest="name"
+    )
+
+    # delete-repository function parser
+    parser_delete_repository = subparsers.add_parser(
+        "delete-repository", help="Delete repository"
+    )
+    parser_delete_repository.add_argument(
+        "-n", "--name", help="Repository name to delete", required=True, dest="name"
+    )
+
     args = parser.parse_args()
 
-    if args.command == "create-repository":
-        create_repository(args.name)
-    elif args.command == "get-repository":
+    if args.command == "get-repository":
         get_repository(args.name)
+    elif args.command == "list-repository":
+        list_repository()
+    elif args.command == "create-repository":
+        create_repository(args.name)
     elif args.command == "delete-repository":
         delete_repository(args.name)
-    elif args.command == "list-repository":
-        list_repository(args.username)
     else:
         return False
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-    
