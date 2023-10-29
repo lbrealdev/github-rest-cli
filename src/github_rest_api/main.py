e679c927d0ff0878de3e0548dd93f49073f32c59
lbrealdev <lbrealdeveloper@gmail.com>
2023-10-29 02:50:13 +0100

    feat: refactor create and delere repository functions and other improvements

--- src/github_rest_api/main.py
+++ src/github_rest_api/main.py
@@ -35,9 +35,7 @@ def get_repository(name: str, org: str):
             source_repo = json.loads(req.text)
             rprint(source_repo)
         elif org is not None:
-            req = requests.get(
-                f"{GITHUB_URL}/repos/{org}/{name}", headers=HEADERS
-            )
+            req = requests.get(f"{GITHUB_URL}/repos/{org}/{name}", headers=HEADERS)
             req.raise_for_status()
             source_repo = json.loads(req.text)
             rprint(source_repo)
@@ -55,45 +53,28 @@ def get_repository(name: str, org: str):
             )
         else:
             rich_output(
-                f"Failed to get repository {name}\n" +
-                f"Status code: {str(e.response.status_code)}",
-                fmt="blink bold red"
+                f"Failed to get repository {name}\n"
+                + f"Status code: {str(e.response.status_code)}",
+                fmt="blink bold red",
             )
 
 
-def create_repository(name: str, private: str, org: str):
-    if private == 'true':
-        is_private = True
-    elif private == 'false':
-        is_private = False
-    else:
-        is_private = False
-
+def create_repository(name: str, visibility: str, org: str):
     data = {
         "name": name,
         "auto_init": "true",
-        "private": is_private,
+        "visibility": visibility,
+        "private": True if visibility == "private" else False
     }
 
     try:
-        if org is not None:
-            req = requests.post(
-                f"{GITHUB_URL}/orgs/{org}/repos", headers=HEADERS, json=data
-            )
-            req.raise_for_status()
-            rich_output(
-                f"Repository sucessfully created in {org}/{name}",
-                fmt="blink bold green",
-            )
-        else:
-            req = requests.post(
-                f"{GITHUB_URL}/user/repos", headers=HEADERS, json=data
-            )
-            req.raise_for_status()
-            rich_output(
-                f"Repository sucessfully created in {GITHUB_USER}/{name}", 
-                fmt="blink bold green",
-            )
+        url = f"{GITHUB_URL}/orgs/{org}/repos" if org else f"{GITHUB_URL}/user/repos"
+        req = requests.post(url, headers=HEADERS, json=data)
+        req.raise_for_status()
+        rich_output(
+            f"Repository successfully created in {org or GITHUB_USER}/{name}",
+            fmt="blink bold green",
+        )
     except requests.exceptions.HTTPError as e:
         if e.response.status_code == 401:
             rich_output(
@@ -103,36 +84,29 @@ def create_repository(name: str, private: str, org: str):
         elif e.response.status_code == 422:
             rich_output(
                 "Repository name already exists on this account or organization!",
-                fmt="blink bold red"
+                fmt="blink bold red",
             )
         else:
             rich_output(
-                f"Failed to create repository {name}" +
-                f"Status code: {e.response.status_code}",
+                f"Failed to create repository {name}\
+                    Status code: {e.response.status_code}",
                 fmt="blink bold red",
             )
 
 
 def delete_repository(name: str, org: str):
     try:
-        if org is not None:
-            req = requests.delete(
-                f"{GITHUB_URL}/repos/{org}/{name}", headers=HEADERS
-            )
-            req.raise_for_status()
-            rich_output(
-                f"Repository sucessfully deleted in {org}/{name}",
-                fmt="blink bold green"
-            )
-        else:
-            req = requests.delete(
-                f"{GITHUB_URL}/repos/{GITHUB_USER}/{name}", headers=HEADERS
-            )
-            req.raise_for_status()
-            rich_output(
-                f"Repository sucessfully deleted in {GITHUB_USER}/{name}", 
-                fmt="blink bold green",
-            )
+        url = (
+            f"{GITHUB_URL}/repos/{org}/{name}"
+            if org
+            else f"{GITHUB_URL}/repos/{GITHUB_USER}/{name}"
+        )
+        req = requests.delete(url, headers=HEADERS)
+        req.raise_for_status()
+        rich_output(
+            f"Repository sucessfully deleted in {org or GITHUB_USER}/{name}",
+            fmt="blink bold green",
+        )
     except requests.exceptions.HTTPError as e:
         if e.response.status_code == 403:
             rich_output(
@@ -141,7 +115,8 @@ def delete_repository(name: str, org: str):
             )
         elif e.response.status_code == 404:
             rich_output(
-                "The requested repository was not found!", fmt="blink bold red",
+                "The requested repository was not found!",
+                fmt="blink bold red",
             )
         else:
             rich_output(
@@ -154,9 +129,7 @@ def delete_repository(name: str, org: str):
 def list_repositories(page: int, property: str, role: str):
     try:
         params = {"per_page": page, "sort": property, "type": role}
-        req = requests.get(
-            f"{GITHUB_URL}/user/repos", headers=HEADERS, params=params
-        )
+        req = requests.get(f"{GITHUB_URL}/user/repos", headers=HEADERS, params=params)
         req.raise_for_status()
         repositories = json.loads(req.text)
         repository_full_name = [repo["full_name"] for repo in repositories]
@@ -171,12 +144,11 @@ def list_repositories(page: int, property: str, role: str):
         if e.response.status_code == 401:
             rich_output(
                 "Unauthorized access. Please check your token or credentials.",
-                fmt="blink bold red", 
+                fmt="blink bold red",
             )
         else:
             rich_output(
-                f"Failed to list repositories for {GITHUB_USER}\n" +
-                f"Status code: {e.response.status_code}",
+                f"Failed to list repositories for {GITHUB_USER} Status code: {e.response.status_code}",
                 fmt="blink bold red",
             )
 
@@ -203,7 +175,7 @@ def dependabot_security(name: str, org: str, enabled: bool):
             req.raise_for_status()
             rich_output(
                 f"Disable dependabot on repository: {org}/{name}",
-                fmt="blink bold green"
+                fmt="blink bold green",
             )
         else:
             if org is None and is_enabled is True:
@@ -225,7 +197,7 @@ def dependabot_security(name: str, org: str, enabled: bool):
                 req.raise_for_status()
                 rich_output(
                     f"Disable dependabot on repository: {GITHUB_USER}/{name}",
-                    fmt="blink bold green" 
+                    fmt="blink bold green",
                 )
     except requests.exceptions.RequestException as e:
         rprint(f"Error: {e}")
@@ -240,9 +212,9 @@ def deployment_environment(name: str, env: str, org: str):
             )
             req.raise_for_status()
             rich_output(
-                f"New deployment environment created - {env.upper()}\n" +
-                f"Repository: {org}/{name}",
-                fmt="blink bold green"
+                f"New deployment environment created - {env.upper()}\n"
+                + f"Repository: {org}/{name}",
+                fmt="blink bold green",
             )
         elif env != "":
             req = requests.put(
@@ -251,16 +223,15 @@ def deployment_environment(name: str, env: str, org: str):
             )
             req.raise_for_status()
             rich_output(
-                f"New deployment environment created - {env.upper()}\n" +
-                f"Repository: {GITHUB_USER}/{name}",
-                fmt="blink bold green"
+                f"New deployment environment created - {env.upper()}\n"
+                + f"Repository: {GITHUB_USER}/{name}",
+                fmt="blink bold green",
             )
     except requests.exceptions.HTTPError as e:
         if e.response.status_code == 422:
             rich_output(
-                f"Failed to create environment {env.upper()}",
-                fmt="blink bold red"
-            )    
+                f"Failed to create environment {env.upper()}", fmt="blink bold red"
+            )
         else:
             rprint(f"Error: {e}")
 
@@ -288,16 +259,12 @@ def cli():
         dest="name",
     )
     get_repo_parser.add_argument(
-        "-o",
-        "--org",
-        help="The organization name",
-        required=False,
-        dest="org"
+        "-o", "--org", help="The organization name", required=False, dest="org"
     )
 
     # Subparser for "list-repository" function
     list_repo_parser = subparsers.add_parser(
-        "list-repo", 
+        "list-repo",
         help="List repositories for authenticated user",
     )
     list_repo_parser.add_argument(
@@ -327,22 +294,22 @@ def cli():
 
     # Subparser for "create-repository" function
     create_repo_parser = subparsers.add_parser(
-        "create-repo", 
+        "create-repo",
         help="Create a new repository",
     )
     create_repo_parser.add_argument(
-        "-n", 
-        "--name", 
-        required=True, 
+        "-n",
+        "--name",
+        required=True,
         dest="name",
-        help="The repository name", 
+        help="The repository name",
     )
     create_repo_parser.add_argument(
-        "-p",
-        "--private",
+        "-v",
+        "--visibility",
         required=False,
-        default=None,
-        dest="private",
+        default="public",
+        dest="visibility",
         help="Whether the repository is private",
     )
     create_repo_parser.add_argument(
@@ -355,7 +322,7 @@ def cli():
 
     # Subparser for "delete-repository" function
     delete_repo_parser = subparsers.add_parser(
-        "delete-repo", 
+        "delete-repo",
         help="Delete a repository",
     )
     delete_repo_parser.add_argument(
@@ -375,7 +342,7 @@ def cli():
 
     # Subparser for "delete-repository" function
     dependabot_parser = subparsers.add_parser(
-        "dependabot", 
+        "dependabot",
         help="Github Dependabot security updates",
     )
     dependabot_parser.add_argument(
@@ -404,11 +371,11 @@ def cli():
         action="store_false",
         dest="control",
         help="Disable dependabot security updates",
-    )    
+    )
 
     # Subparser for "deployment-environments" function
     deploy_env_parser = subparsers.add_parser(
-        "environment", 
+        "environment",
         help="Github Deployment environments",
     )
     deploy_env_parser.add_argument(
@@ -442,7 +409,7 @@ def cli():
     if command == "list-repo":
         return list_repositories(args.page, args.sort, args.role)
     if command == "create-repo":
-        return create_repository(args.name, args.private, args.org)
+        return create_repository(args.name, args.visibility, args.org)
     if command == "delete-repo":
         return delete_repository(args.name, args.org)
     if command == "dependabot":
