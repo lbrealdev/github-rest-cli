08e4a6ad9eb9c6b7d482e7d87241d5c3303ed76a
lbrealdev <lbrealdeveloper@gmail.com>
2025-06-17 17:28:11 +0200

    feat: refactor tests for create repository function

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,53 +1,43 @@
 from github_rest_cli import api
 
 
+GET_HEADERS_FUNCTION = "github_rest_cli.api.get_headers"
+FETCH_USER_FUNCTION = "github_rest_cli.api.fetch_user"
+REQUEST_HANDLER_FUNCTION = "github_rest_cli.api.request_with_handling"
+
+
 def test_fetch_user(mocker):
-    mocker.patch(
-        "github_rest_cli.api.get_headers", return_value={"Authorization": "token fake"}
-    )
+    mocker.patch(GET_HEADERS_FUNCTION, return_value={"Authorization": "token fake"})
 
     mock_response = mocker.Mock()
     mock_response.status_code = 200
     mock_response.json.return_value = {"login": "test-user"}
     mock_response.raise_for_status = lambda: None
 
-    mocker.patch(
-        "github_rest_cli.api.request_with_handling", return_value=mock_response
-    )
+    mocker.patch(REQUEST_HANDLER_FUNCTION, return_value=mock_response)
 
     result = api.fetch_user()
 
     assert result == "test-user"
 
 
-def test_create_repository_user(mocker):
-    mocker.patch(
-        "github_rest_cli.api.get_headers", return_value={"Authorization": "token fake"}
-    )
-
-    mocker.patch("github_rest_cli.api.fetch_user", return_value="test-user")
-
+def test_create_repository_user_expected_message(mocker):
     expected_message = "Repository successfully created in test-user/test-repo."
 
-    mocker.patch(
-        "github_rest_cli.api.request_with_handling", return_value=expected_message
-    )
+    mocker.patch(GET_HEADERS_FUNCTION, return_value={"Authorization": "token fake"})
+    mocker.patch(FETCH_USER_FUNCTION, return_value="test-user")
+    mocker.patch(REQUEST_HANDLER_FUNCTION, return_value=expected_message)
 
     result = api.create_repository("test-repo", "public")
 
     assert result == expected_message
 
 
-def test_create_repository_org(mocker):
-    mocker.patch(
-        "github_rest_cli.api.get_headers", return_value={"Authorization": "token fake"}
-    )
-
+def test_create_repository_org_expected_message(mocker):
     expected_message = "Repository successfully created in test-org/test-repo."
 
-    mocker.patch(
-        "github_rest_cli.api.request_with_handling", return_value=expected_message
-    )
+    mocker.patch(GET_HEADERS_FUNCTION, return_value={"Authorization": "token fake"})
+    mocker.patch(REQUEST_HANDLER_FUNCTION, return_value=expected_message)
 
     result = api.create_repository("test-repo", "public", "test-org")
 
